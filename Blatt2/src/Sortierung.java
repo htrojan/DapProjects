74f0a5e1225e6552442c532c56d6d707b29982d4
The-Ludwig <ludwig.neste@tu-dortmund.de>
2018-04-26 08:24:33 +0200

    Final version (With dumb merge)

--- Blatt2/src/Sortierung.java
+++ Blatt2/src/Sortierung.java
@@ -205,6 +205,7 @@ public class Sortierung {
         default:
         				//Ungueltiger Parameter
         				printUsage();
+        				return;
         }
         
         end = System.currentTimeMillis();
