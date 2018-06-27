c61b1cfa568553bad241518b1c811fd3818ce2c5
lobbi44 <hendyGA@posteo.de>
2018-06-27 21:36:03 +0200

    Implemented Heap

---
+++ Blatt11/src/Heap.java
@@ -0,0 +1,102 @@
+public class Heap {
+    private int fillCount; //heapSize ist ein irreführender Name
+    private int[] keys;
+
+    public Heap(int size) {
+        keys = new int[size];
+        fillCount = 0;
+    }
+
+    public int left(int i) {
+        //Plausibilität: n = 2^h -1
+        //--> D.h. 2*n = n + 2^h -1 , also ein Element vor Abschluss (also das vorletzte), d.h. das Linke
+        //Davon ausgehend ist jedes n weniger eine Verringerung von -2 in 2*n d.h. alle nachfolgenden sind auch
+        //linke Knoten, da nur gerade Schritte. "Quadrat" (Die mögliche Induktion ist dem geneigten Leser überlassen)
+        return (i + 1) * 2 - 1;
+    }
+
+    public int right(int i) {
+        //Offensichtlicherweise das einen weiter rechts vom linken...
+        return (i + 1) * 2;
+    }
+
+    public int parent(int i) {
+        //Immer abgerundet, da Integer
+        return ((i + 1) / 2) - 1;
+    }
+
+    /**
+     * Stellt rekursiv von unten nach oben Heapeigenschaft wieder her
+     *
+     * @param i
+     */
+    public void heapify(int i) {
+
+        if (i >= fillCount || i < 0)
+            return; //Über oder unter max. bzw. min. Index --> Rekursionsabbruch
+
+        int lc = left(i);
+        int rc = right(i);
+
+        int max = 0;
+
+        if (lc >= fillCount) { //Hat keine Kinder, Blatt --> Rekursiv bis nach oben aufrufen
+            heapify(parent(i));
+            return;
+        } else if (rc >= fillCount) { //Hat nur linkes Kind --> Größtes Kind gefunden
+            max = lc;
+        } else {
+            //Das Maximum der Schlüssel suchen
+            assert (keys[lc] != keys[rc]); //Kein Schlüssel darf doppelt sein
+            if (keys[lc] > keys[rc])
+                max = lc;
+            else
+                max = rc;
+        }
+
+        //Das Maximum "hochtauschen" auf aktuelle Position falls größer als aktueller Wert
+        if (keys[i] < keys[max])
+            swap(i, max);
+        heapify(parent(i));
+
+    }
+
+    public void insert(int key) {
+        if (fillCount >= keys.length) {
+            throw new IllegalStateException("Can't insert key if heap is full");
+        }
+
+        keys[fillCount++] = key; //Schon hier hochzählen, da heapify() einen aktuellen Wert braucht
+        heapify(fillCount - 1);
+    }
+
+    public int extractMax() {
+        if (fillCount < 1) {
+            new IllegalStateException("Heap needs entries to extract maximum");
+        }
+        //Maximum steht immer oben
+        return keys[0];
+    }
+
+    public void printHeap() {
+        int maxColumn = 1;
+
+        for (int i = 1; i <= fillCount; i++) {
+            if (i == maxColumn) {
+                System.out.println(); //Leerzeile
+                maxColumn *= 2;
+            }
+
+            System.out.print(keys[i - 1] + "  ");
+        }
+    }
+
+    /**
+     * Swaps two elements at positions i1 and i2
+     */
+    private void swap(int i1, int i2) {
+        int tmp = keys[i1];
+        keys[i1] = keys[i2];
+        keys[i2] = tmp;
+    }
+}
---
+++ Blatt11/src/TestHeap.java
@@ -0,0 +1,14 @@
+public class TestHeap {
+
+    public static void main(String[] args) {
+        int[] add = new int[]{3, 9, 12, 15, 14, 13, 5};
+
+        Heap heap = new Heap(50);
+
+        for (int i : add) {
+            heap.insert(i);
+            System.out.println("\n");
+            heap.printHeap();
+        }
+    }
+}
